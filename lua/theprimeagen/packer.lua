cb4542106ee3c046d701f84cd55392599792a767
biembouash <pedrodamatta2020@gmail.com>
2024-09-13 12:56:01 +0100

    nvim nerd tree added

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -15,6 +15,8 @@ return require('packer').startup(function(use)
 			vim.cmd('colorscheme rose-pine')
 		end
 	})
+    use('nvim-tree/nvim-tree.lua')
+    use('nvim-tree/nvim-web-devicons')
 	use('nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'})
 	use('ThePrimeagen/harpoon')
 	use({'VonHeikemen/lsp-zero.nvim', 
--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -30,3 +30,21 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "160"
 
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
